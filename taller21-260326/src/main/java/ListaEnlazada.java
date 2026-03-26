e826160c80ce8ef08ea0c7a1f3272c78fd766558
Juan Felipe Garzon Varon <est.juanf.garzon@unimilitar.edu.co>
2026-03-26 10:22:32 -0500

    creo la clase listaenlazada con la lista y el metodo para ordenar la lista ingresada

---
+++ taller21-260326/src/main/java/ListaEnlazada.java
@@ -0,0 +1,63 @@
+class ListaEnlazada {
+    Nodo cabeza;
+
+    public void agregar(int dato) {
+        Nodo nuevo = new Nodo(dato);
+
+   
+    
+        Nodo temp = cabeza;
+        while (temp.siguiente != null) {
+            temp = temp.siguiente;
+        }
+        temp.siguiente = nuevo;
+    }
+
+    public void ordenar() {
+
+        
+        boolean cambio;
+
+        do {
+            cambio = false;
+            Nodo actual = cabeza;
+
+            
+            while (actual.siguiente != null) {
+                if (actual.dato >= actual.siguiente.dato) {
+
+                    int temp = actual.dato;
+                    actual.dato = actual.siguiente.dato;
+                    actual.siguiente.dato = temp;
+
+                    cambio = true;
+                }
+                actual = actual.siguiente;
+            }
+
+        } while (cambio);
+    }
+
+    public int suma() {
+        int suma = 0;
+        Nodo temp = cabeza;
+
+       
+        while (temp.siguiente != null) {
+            suma += temp.dato;
+            temp = temp.siguiente;
+        }
+
+        return suma;
+    }
+
+    public void mostrar() {
+        Nodo temp = cabeza;
+
+        while (temp != null) {
+            System.out.print(temp.dato + " ");
+            temp = temp.siguiente;
+        }
+        System.out.println();
+    }
+}
---
+++ taller21-260326/src/main/java/Taller21260326.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Taller21260326 {
+
+    public static void main(String[] args) {
+
+     
+    }
+}
