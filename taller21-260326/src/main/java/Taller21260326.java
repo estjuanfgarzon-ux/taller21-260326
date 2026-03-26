955bd22b9d45b331567b392af4bcb64d7a2e8bde
Juan Felipe Garzon Varon <est.juanf.garzon@unimilitar.edu.co>
2026-03-26 10:38:58 -0500

    Agrego a la clase taller lo necesario para imprimir y demas

--- taller21-260326/src/main/java/Taller21260326.java
+++ taller21-260326/src/main/java/Taller21260326.java
@@ -3,6 +3,37 @@ public class Taller21260326 {
 
     public static void main(String[] args) {
 
-     
+      Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingrese el tamaño:");
+        int n = sc.nextInt();
+
+        ListaEnlazada lista = new ListaEnlazada();
+
+        System.out.println("Ingrese " + (n - 1) + " numeros (falta uno):");
+
+    
+        for (int i = 0; i < n; i++) {
+            lista.agregar(sc.nextInt());
+        }
+
+        System.out.println("Lista original:");
+        lista.mostrar();
+
+        lista.ordenar();
+
+        System.out.println("Lista ordenada:");
+        lista.mostrar();
+
+        int sumaLista = lista.suma();
+
+        
+        int esperado = n * (n + 1);
+
+        
+        int faltante = sumaLista - esperado;
+
+       
+        System.out.println("El numero faltante es: " + sumaLista);
     }
 }
