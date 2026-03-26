6458d20b3a0e0635a2b0c3f131be40eacb771024
Juan Felipe Garzon Varon <est.juanf.garzon@unimilitar.edu.co>
2026-03-26 10:33:01 -0500

    agrego un if ya que no validaba si la lista estaba vacia

--- taller21-260326/src/main/java/ListaEnlazada.java
+++ taller21-260326/src/main/java/ListaEnlazada.java
@@ -5,7 +5,10 @@ class ListaEnlazada {
         Nodo nuevo = new Nodo(dato);
 
    
-    
+     if (cabeza == null) {
+       cabeza = nuevo;
+          return;
+      }
         Nodo temp = cabeza;
         while (temp.siguiente != null) {
             temp = temp.siguiente;
